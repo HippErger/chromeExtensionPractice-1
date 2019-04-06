ebce276085da207872f1a8b7420e4f72251dd301
HippErger <berger.clayton@gmail.com>
2019-04-06 15:46:20 -0500

    removed broken code

--- popup.js
+++ popup.js
@@ -5,29 +5,30 @@ let changeColor = document.getElementById('changeColor');
     changeColor.setAttribute('value', data.color);
   });
 
-  changeColor.onclick = function(element) {
-    let color = element.target.value;
-    let color2 = 'white';
-    chrome.tabs.query({active: true, currentWindow: true}, function(tabs) {
-      if(document.body.style.backgroundColor === color) {
-        console.log('should change to white')
-        chrome.tabs.executeScript(
-          tabs[0].id,
-          {code: 'document.body.style.backgroundColor = "' + color2 + '";'});
-      } else {
-        console.log('Should change to green')
-        chrome.tabs.executeScript(
-          tabs[0].id,
-          {code: 'document.body.style.backgroundColor = "' + color + '";'});
-      }
-    });
-  };
 
-  /**  changeColor.onclick = function(element) {
-    let color = element.target.value;
-    chrome.tabs.query({active: true, currentWindow: true}, function(tabs) {
-      chrome.tabs.executeScript(
-          tabs[0].id,
-          {code: 'document.body.style.backgroundColor = "' + color + '";'});
-    });
-  }; */
+  // * Trying to switch colors on each click. Doesn't work this. 
+  // I don't think we have access to document or window or console 
+  // directly through an extension. 
+  // * I see now that this script is only loaded once into the 
+  // *
+  
+  // button that rendered once. It can't make changes to the DOM itself. 
+  // changeColor.onclick = function(element) {
+  //   let color = element.target.value;
+
+  //   chrome.tabs.query({active: true, currentWindow: true}, function(tabs) {
+  //     if(document.body.style.backgroundColor === color) { 
+  //       chrome.storage.sync.set({color: "white"}, function(data) {
+  //         console.log('color is ' + item);
+  //       })
+  //       chrome.tabs.executeScript(
+  //         tabs[0].id,
+  //         {code: 'document.body.style.backgroundColor = "black"'});
+  //     } else {
+  //       chrome.tabs.executeScript(
+  //         tabs[0].id,
+  //         {code: 'document.body.style.backgroundColor = "' + color + '";'});
+  //     }
+  //   });
+  // };
+
