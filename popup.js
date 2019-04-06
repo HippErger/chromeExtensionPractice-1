752cd919862593dbdf7d22d41b87314c5696e7cc
HippErger <berger.clayton@gmail.com>
2019-04-06 14:59:36 -0500

    broken change color function :(

--- popup.js
+++ popup.js
@@ -4,3 +4,30 @@ let changeColor = document.getElementById('changeColor');
     changeColor.style.backgroundColor = data.color;
     changeColor.setAttribute('value', data.color);
   });
+
+  changeColor.onclick = function(element) {
+    let color = element.target.value;
+    let color2 = 'white';
+    chrome.tabs.query({active: true, currentWindow: true}, function(tabs) {
+      if(document.body.style.backgroundColor === color) {
+        console.log('should change to white')
+        chrome.tabs.executeScript(
+          tabs[0].id,
+          {code: 'document.body.style.backgroundColor = "' + color2 + '";'});
+      } else {
+        console.log('Should change to green')
+        chrome.tabs.executeScript(
+          tabs[0].id,
+          {code: 'document.body.style.backgroundColor = "' + color + '";'});
+      }
+    });
+  };
+
+  /**  changeColor.onclick = function(element) {
+    let color = element.target.value;
+    chrome.tabs.query({active: true, currentWindow: true}, function(tabs) {
+      chrome.tabs.executeScript(
+          tabs[0].id,
+          {code: 'document.body.style.backgroundColor = "' + color + '";'});
+    });
+  }; */
